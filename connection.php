5a8692622643fcd9fb107a492dbaabb198759364
MrSpeaker003 <154431028+MrSpeaker003@users.noreply.github.com>
2023-12-20 05:05:59 -0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
 //database connection
-$con= new mysqli('localhost','root','','university')or die("Could not connect to mysql".mysqli_error($con));
+//$con= new mysqli('localhost','root','','university')or die("Could not connect to mysql".mysqli_error($con));
 ?>
